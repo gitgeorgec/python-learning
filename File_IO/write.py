6d8c50a16b0d653c60d87f6f7e80bb3421d000f3
george <ccchang0661@gmail.com>
2019-09-09 23:49:42 +0800

    fileIO part 1

---
+++ File_IO/write.py
@@ -0,0 +1,7 @@
+# with open("haiku.text", "w") as file:
+# 	file.write("Writing files is great\n")
+# 	file.write("Here's another line of text\n")
+# 	file.write("Closing now, goodbye!\n")
+
+with open("lol.text", "w") as file:
+	file.write("hahah\n" * 100)
